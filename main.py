bbbcbffc6f27e5c4c01f36395c06a29481dff4ba
avencores <64781822+AvenCores@users.noreply.github.com>
2026-05-20 18:43:40 +0300

    return

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
 import sys
 import os
-import subprocess
 
 from patcher.utils.admin import is_admin, run_as_admin
 from patcher.utils.console import setup_console
 from patcher.cli import run_cli, confirmed
 
 
-def ensure_linux_terminal():
-    if sys.platform != 'linux':
-        return
-    if not getattr(sys, 'frozen', False):
-        return
-    if (sys.stdout and sys.stdout.isatty()) or (sys.stdin and sys.stdin.isatty()):
-        return
-    
-    terminals = [
-        ['x-terminal-emulator', '-e'],
-        ['gnome-terminal', '--'],
-        ['konsole', '-e'],
-        ['xfce4-terminal', '-x'],
-        ['mate-terminal', '-x'],
-        ['lxterminal', '-e'],
-        ['tilix', '-e'],
-        ['terminator', '-e'],
-        ['xterm', '-e']
-    ]
-    
-    executable = sys.executable
-    args = sys.argv[1:]
-    
-    bash_cmd = '"$0" "$@"; echo; read -p "  Press Enter to exit..."'
-    
-    env = os.environ.copy()
-    if 'LD_LIBRARY_PATH_ORIG' in env:
-        env['LD_LIBRARY_PATH'] = env['LD_LIBRARY_PATH_ORIG']
-    else:
-        env.pop('LD_LIBRARY_PATH', None)
-        
-    for term in terminals:
-        try:
-            subprocess.Popen(term + ['bash', '-c', bash_cmd, executable] + args, env=env)
-            sys.exit(0)
-        except Exception:
-            continue
-
-
 if __name__ == "__main__":
-    ensure_linux_terminal()
     setup_console()
     if os.name == "nt" and not is_admin():
         if run_as_admin():
